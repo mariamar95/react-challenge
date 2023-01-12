4cfee1abbe626bf11b5b18dddaebdb1f4a990c5e
Maria <mariamar_@hotmail.gr>
2023-01-12 12:10:23 +0000

    linked css to sidebar.js

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -1,10 +1,11 @@
 import React from "react"
+import css from './css/Sidebar.module.css';
 
 
 const Sidebar = (props) => {
     console.log(props);
     return (
-        <div>
+        <div className = {css.sidebar}>
             <a href="#" target="_blank">My Photos</a>
             <a href="#" target="_blank">My Illustrations</a>
             <a href="#" target="_blank">My Partings</a>
